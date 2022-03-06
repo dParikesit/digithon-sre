526306d6bf8f8c4038c080dcf75f1002079c6cf9
Ariq Ramdhany <ariqathallah38@gmail.com>
2022-03-06 10:01:55 +0700

    Updated to Node v17.x

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,12 @@
-import { ChatFeed, ChatBubble, BubbleGroup, Message } from "react-chat-ui";
-import { useState } from "react";
+import {
+  ChatFeed,
+  ChatBubble,
+  BubbleGroup,
+  Message
+} from "react-chat-ui";
+import {
+  useState
+} from "react";
 import React from "react";
 import "./App.css";
 
@@ -31,15 +38,22 @@ const users = {
   2: "Evan",
 };
 
-const customBubble = (props) => (
-  <div>
-    <p>{`${props.message.senderName} ${props.message.id ? "says" : "said"}: ${props.message.message}`}</p>
-  </div>
+const customBubble = (props) => ( <
+  div >
+  <
+  p > {
+    `${props.message.senderName} ${props.message.id ? "says" : "said"}: ${props.message.message}`
+  } < /p> <
+  /div>
 );
 
 function App() {
   const [messages, setMessages] = useState([
-    new Message({ id: 1, message: "Halo guys, PR Mat nomor 5 jawabannya apa ya?", senderName: "Dimas" }),
+    new Message({
+      id: 1,
+      message: "Halo guys, PR Mat nomor 5 jawabannya apa ya?",
+      senderName: "Dimas"
+    }),
     new Message({
       id: 2,
       message: "Wah, aku juga belum tuh",
@@ -68,7 +82,9 @@ function App() {
 
   async function pushMessage(recipient, message) {
     console.log(message);
-    let obj = { sentence: message };
+    let obj = {
+      sentence: message
+    };
     console.log(obj);
     let response = await fetch("http://localhost:8080/query", {
       mode: "cors",
@@ -89,7 +105,7 @@ function App() {
     });
 
     if (response === true) {
-      let isApproved = window.confirm(`Apakah kamu yakin akan mengirim kalimat "${message}"?`);
+      let isApproved = window.confirm(`Kalimat yang kamu tulis mengandung unsur toksik. Apakah kamu yakin akan mengirim kalimat "${message}"?`);
       if (isApproved) {
         let listMessages = [...messages, newMessage];
         setMessages(listMessages);
@@ -100,29 +116,48 @@ function App() {
       setMessages(listMessages);
     }
   }
-  return (
-    <div className="container">
-      <h1 className="text-center">kindboard</h1>
-      <div className="chatfeed-wrapper">
-        <ChatFeed
-          chatBubble={useCustomBubble && customBubble}
-          maxHeight={250}
-          messages={messages} // Boolean: list of message objects
-          showSenderName
-        />
+  return ( <
+    div className = "container" >
+    <
+    h1 className = "text-center" > kindboard < /h1> <
+    div className = "chatfeed-wrapper" >
+    <
+    ChatFeed chatBubble = {
+      useCustomBubble && customBubble
+    }
+    maxHeight = {
+      250
+    }
+    messages = {
+      messages
+    } // Boolean: list of message objects
+    showSenderName /
+    >
 
-        <form onSubmit={(e) => onMessageSubmit(e)}>
-          <input
-            ref={(m) => {
-              setMessage(m);
-            }}
-            placeholder="Type a message..."
-            className="message-input"
-          />
-        </form>
-        <div style={{ display: "flex", justifyContent: "center", marginTop: 10 }}></div>
-      </div>
-    </div>
+    <
+    form onSubmit = {
+      (e) => onMessageSubmit(e)
+    } >
+    <
+    input ref = {
+      (m) => {
+        setMessage(m);
+      }
+    }
+    placeholder = "Type a message..."
+    className = "message-input" /
+    >
+    <
+    /form> <
+    div style = {
+      {
+        display: "flex",
+        justifyContent: "center",
+        marginTop: 10
+      }
+    } > < /div> <
+    /div> <
+    /div>
   );
 }
 
